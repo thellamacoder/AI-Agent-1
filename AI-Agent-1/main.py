a5aa1d52f6715af7397497e47d4a918004777894
thellamacoder <scott.s.klein@gmail.com>
2025-07-25 09:41:02 -0400

    used solution again to complete CH1 L6

--- AI-Agent-1/main.py
+++ AI-Agent-1/main.py
@@ -1,23 +1,53 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 
 def main():
-    print("Hello from ai-agent-1!")
 
+    load_dotenv()
+
+    # The line below is equivalent to if "--verbose" in sys.argv: verbose = True else: verbose = False
+    verbose = "--verbose" in sys.argv
+
+    # Taken from the boot.dev solution. Not sure how I am supposed to know how to do that from these courses.
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+
+        print("AI Code Assistant")
+        print("\nUsage: python main.py 'your prompt here' [--verbose]")
+        print("Example: python main.py 'How do I build a calculator app?'")
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+    
+    messages = [types.Content(role="user", 
+        parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(model = "gemini-2.0-flash-001",
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-
+        contents = messages,)
+    
+    if verbose:
+        print(f"Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print(f"Response tokens: ", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()
