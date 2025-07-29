3649f2dea323e56f431a74c6bb5e1dceabe36705
thellamacoder <scott.s.klein@gmail.com>
2025-07-29 09:31:32 -0400

    _completed CH2 L3 with assistance from the solution files

--- AI-Agent-1/functions/get_file_content.py
+++ AI-Agent-1/functions/get_file_content.py
@@ -1,15 +1,22 @@
 import os
-from functions.config import MAX_CHARS
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_directory = os.path.abspath(working_directory)
-    target_file = os.path.abspath(os.path.join(abs_working_directory, file_path))
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not target_file.startswith(abs_working_directory):
+    if not abs_file_path.startswith(abs_working_directory):
         return f"Error: Cannot read '{file_path}' as it is outside the permitted working directory"
     
-    if not os.path.isfile(target_file):
+    if not os.path.isfile(abs_file_path):
         return f"Error: File not found or is not a regular file: '{file_path}'"
-    
-    with open(file_path, "r") as f:
-        file_content_string = f.read(MAX_CHARS)
+
+    try:    
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                file_content_string += (f"[...File '{file_path}' truncated at {MAX_CHARS} characters]")
+            return file_content_string
+
+    except Exception as e:
+        return f"Error reading file '{file_path}': {e}"
--- AI-Agent-1/tests.py
+++ AI-Agent-1/tests.py
@@ -3,9 +3,25 @@ from functions.get_file_content import get_file_content
 
 def test():
 
-    result = get_file_content("/calculator", "main.py")
-    print("Result for file content:")
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for calculator/lorem.txt file content:")
     print(result)
 
+    result = get_file_content("calculator", "main.py")
+    print("Result for calculator/main.py:")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for calculator/pkg/calculator.py:")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result from calculator/bin/cat")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result from calculator/pkg/does_not_exixt.py:")
+    print(result)
+    
 if __name__ == "__main__":
     test()
