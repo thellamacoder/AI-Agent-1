fc39bc64c609a7c01ebd1924054e1b06be4c17ff
thellamacoder <scott.s.klein@gmail.com>
2025-08-18 12:54:22 -0400

    Completed CH3L3

--- AI-Agent-1/call_function.py
+++ AI-Agent-1/call_function.py
@@ -1,9 +1,12 @@
 from google.genai import types
 
-from schemas import schema_get_files_info
+from schemas import *
 
 available_functions = types.Tool(
 function_declarations=[
 schema_get_files_info,
+schema_get_file_content,
+schema_run_python_file,
+schema_write_file,
 ]
 )
--- AI-Agent-1/schemas.py
+++ AI-Agent-1/schemas.py
@@ -14,3 +14,53 @@ schema_get_files_info = types.FunctionDeclaration(
         },
     ),
 )
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Lists the content of the files specified as long as they are located in the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path from which to list files, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file located in the working directory. Times out after 30 seconds.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+              type=types.Type.STRING,
+              description="The path to the target Python file to execute.",
+            ),
+            "args": types.Schema(
+                type=types.Type.STRING,
+                description="List of strings to provide arguments for the Python function being called.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Create and/or overwrite text to a file in the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the target file to write to."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The text string to write to the file in the working directory.",
+            ),
+        },
+    ),
+)
