071b14c9126266f989d361aeb0ba37394fdf4270
thellamacoder <scott.s.klein@gmail.com>
2025-08-19 12:21:20 -0400

    _began CH3 L4

--- AI-Agent-1/call_function.py
+++ AI-Agent-1/call_function.py
@@ -10,3 +10,18 @@ schema_run_python_file,
 schema_write_file,
 ]
 )
+
+def call_function(function_call_part, verbose=False):
+
+    function_name = function_call_part.name
+    function_arguments = function_call_part.args
+    function_arguments["working_directory"] = "./calculator"
+
+    
+
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    return function_name(**function_arguments)
