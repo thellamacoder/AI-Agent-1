b1cc6b8f8ef16378936f0255bce4933a6935b886
thellamacoder <scott.s.klein@gmail.com>
2025-08-23 21:19:15 -0400

    CH3 L4 Assignment 5

--- AI-Agent-1/call_function.py
+++ AI-Agent-1/call_function.py
@@ -1,5 +1,5 @@
 from google.genai import types
-
+from functions import *
 from schemas import *
 
 available_functions = types.Tool(
@@ -11,17 +11,40 @@ schema_write_file,
 ]
 )
 
+functions_dict = {"get_files_info":functions.get_files_info, "get_file_content":functions.get_file_content, "run_python": functions.run_python, "write_file":functions.write_file}
+
 def call_function(function_call_part, verbose=False):
 
     function_name = function_call_part.name
     function_arguments = function_call_part.args
     function_arguments["working_directory"] = "./calculator"
+    function = functions_dict[function_name]
 
-    
+    if function_name not in functions_dict:
+        return types.Content(
+        role="tool",
+            parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"error": f"Unknown function: {function_name}"},
+            )
+        ],
+    )
+                              
 
     if verbose:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(f" - Calling function: {function_call_part.name}")
 
-    return function_name(**function_arguments)
+    function_result = function(**function_arguments)
+
+    return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+        )
+    ],
+)
--- AI-Agent-1/main.py
+++ AI-Agent-1/main.py
@@ -6,7 +6,7 @@ from google.genai import types
 from functions import get_files_info
 from prompts import system_prompt
 from schemas import *
-from call_function import available_functions
+from call_function import *
 
 
 def main():
@@ -48,18 +48,22 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(model = "gemini-2.0-flash-001",
         contents = messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
     
-    if verbose:
-        print(f"Prompt tokens: ", response.usage_metadata.prompt_token_count)
-        print(f"Response tokens: ", response.usage_metadata.candidates_token_count)
-    
-    if not response.function_calls:
-        print("Response:")
-        print(response.text)
+
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
+        #print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
+        function_call_result = call_function(function_call_part, verbose)
 
+        try:
+            function_call_result.parts[0].function_response.response
+        
+            if verbose:
+                print(f"->{function_call_result.parts[0].function_response.response}")
+
+        except Exception as e:
+            raise f"Fatal Error: {e}"
+        
 if __name__ == "__main__":
     main()
